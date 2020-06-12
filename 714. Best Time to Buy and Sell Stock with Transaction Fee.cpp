c994a68db1341098165efe7247418980801c40e9
ankuragarwal0408 <40289773+ankuragarwal0408@users.noreply.github.com>
2020-06-12 12:04:57 +0530

    Create 714. Best Time to Buy and Sell Stock with Transaction Fee.cpp

---
+++ 714. Best Time to Buy and Sell Stock with Transaction Fee.cpp
@@ -0,0 +1,30 @@
+class Solution 
+{
+public:
+       int dp[50001][2];
+    int maxselling(vector<int>&prices,int i,int share,int fee )
+    {
+        if(i>=prices.size())
+            return 0;
+        int ans=0;
+        if(dp[i][share]!=-1)
+            return dp[i][share];
+        if(share)// share h mere paas 
+            ans+=max(prices[i]-fee+maxselling(prices,i+1,!share,fee),maxselling(prices,i+1,share,fee));
+        else
+            ans+=max(-prices[i]+maxselling(prices,i+1,!share,fee),maxselling(prices,i+1,share,fee));
+        return dp[i][share]=ans;  
+    }
+    int maxProfit(vector<int>& prices, int fee) 
+    {
+        int n=prices.size();
+        for(int i=0;i<50001;i++)
+         dp[i][0]=-1,dp[i][1]=-1;
+        int dp[prices.size()+1][2];
+        for(int i=0;i<prices.size();i++)
+        {
+            
+        }
+        return maxselling(prices,0,0,fee);
+    }
+};
