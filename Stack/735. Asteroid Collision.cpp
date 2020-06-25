463deafb50f295f61b98b2343a706d2b792c956a
ankuragarwal0408 <40289773+ankuragarwal0408@users.noreply.github.com>
2020-06-25 11:08:07 +0530

    Update 735. Asteroid Collision.cpp

--- Stack/735. Asteroid Collision.cpp
+++ Stack/735. Asteroid Collision.cpp
@@ -5,11 +5,11 @@ public:
         vector<int> s; // use vector to simulate stack.
         for (int i : a) 
         {
-            while (!s.empty() && s.back() > 0 && s.back() < -i)
+            while (!s.empty() && s.back() > 0 && s.back() < -i)// this is cheking if the asteroid at the top is kamzor then negative velocity waala
                 s.pop_back();
-            if (s.empty() || i > 0 || s.back() < 0)
+            if (s.empty() || i > 0 || s.back() < 0)// yadi top waala hi less than zero h toh collision ka koi chance nai h simply push it in the stack
                 s.push_back(i);
-            else if (i < 0 && s.back() == -i)
+            else if (i < 0 && s.back() == -i)// yadi equal waala case usko alag se deal kiya jaa raha h yahan pe
                 s.pop_back();
         }
         return s;
